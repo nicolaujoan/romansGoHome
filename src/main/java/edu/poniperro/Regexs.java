92336e2dc48b10a524ab4a99c99682529c7a7bb2
nicolaujoan <jnicolausantandreu@cifpfb.eu>
2022-03-30 17:42:36 +0200

    add regex and romanNumber modules7

--- src/main/java/edu/poniperro/Regexs.java
+++ src/main/java/edu/poniperro/Regexs.java
@@ -1,5 +1,16 @@
 package edu.poniperro;
 
-public class Regexs {
+import java.util.HashMap;
+import java.util.Map;
 
+public final class Regexs {
+    public static final Map<String, String> regexs = new HashMap<String, String>();
+
+    public void initRegexs() {
+        regexs.put("M", "M{1,3}");
+    }
+
+    public static Map<String, String> getRegexs() {
+        return regexs;
+    }
 }
--- src/main/java/edu/poniperro/RomanNumber.java
+++ src/main/java/edu/poniperro/RomanNumber.java
@@ -1,15 +1,45 @@
 package edu.poniperro;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.regex.*;
+import java.util.stream.Collectors;
 
 public class RomanNumber {
-    public RomanNumber(String roman) {
+    private String roman;
+    private Double decimal;
 
+    public RomanNumber(String roman) {
+        this.roman = roman;
     }
 
     public Double toDecimal() {
+        String regex = getSuitableRegex(roman);
+
+        if (itMatchs(regex)) {
+
+        }
+
         return 0.0;
     }
 
+    private Boolean itMatchs(String regex) {
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(roman);
+        return m.find();
+    }
 
+    private double getDecimalValue(String roman) {
+        String[] strArr = roman.split("");
+
+        for (String ch: strArr) {
+
+        }
+    }
+
+    private static String getSuitableRegex(String roman) {
+        Map<String, String> regexs = Regexs.getRegexs();
+        return regexs.get(roman);
+    }
 }
